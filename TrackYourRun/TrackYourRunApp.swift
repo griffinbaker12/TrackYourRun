94ba169cf9a217b0c4b1f07ca9f0ec26372733ef
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-07-02 22:59:53 -0500

    Initial Commit

---
+++ TrackYourRun/TrackYourRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYourRunApp.swift
+//  TrackYourRun
+//
+//  Created by Griffin Baker on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackYourRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
